f86ccde13945139989d880a0dfc2586530c56572
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-06-28 16:51:01 -0300

    testando o encapsulamento do atributo estoque na classe de modelo produto. Foi tentado inserir um atributo do tipo string no campo que está preparando apenas para receber atributos numéricos e a aplicação lançou uma exceção, conforme esperado

--- classes/model/Produto.php
+++ classes/model/Produto.php
@@ -2,4 +2,12 @@
 
 class Produto extends Record {
 	const TABLENAME = 'produto';
+
+	public function set_estoque($estoque) {
+		if (is_numeric($estoque) AND $estoque > 0) {
+			$this->data['estoque'] = $estoque;
+		} else {
+			throw new Exception("Estoque {$estoque} inválido em ".__CLASS__);
+		}
+	}
 }
