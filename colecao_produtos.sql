a189c0660a04306fb5caeb6e3290987e587dac21
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-07-03 22:19:03 -0300

    created data load file

---
+++ colecao_produtos.sql
@@ -0,0 +1,3 @@
+DELETE FROM produto;
+INSERT INTO produto (descricao, estoque, preco_custo, preco_venda, codigo_barras, data_cadastro, origem) VALUES ('Pendrive 512 Mb', '10.0', '20.0', '40.0', '0000000001', '2015-01-01', 'N');
+INSERT INTO produto (descricao, estoque, preco_custo, preco_venda, codigo_barras, data_cadastro, origem) VALUES ('HD 120 GB', '20.0', '100.0', '180.0', '0000000002', '2015-01-01', 'N');
